7fa6a79f04c81fa5c210d03f6aa98f1f719190ed
Yan Maniez <yanmaniez@hotmail.com>
2018-04-18 22:55:46 +0200

    Ajout début menus déroulants à gauche + liste fonctionnelle avec navigation

--- AideDeJeu/AideDeJeu/App.xaml.cs
+++ AideDeJeu/AideDeJeu/App.xaml.cs
@@ -13,7 +13,7 @@ namespace AideDeJeu
 			InitializeComponent();
 
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
 		protected override void OnStart ()
--- AideDeJeu/AideDeJeu/Views/MainPage.xaml.cs
+++ AideDeJeu/AideDeJeu/Views/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using AideDeJeu.Models;
+using AideDeJeu.ViewModels;
+using System;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -8,9 +10,38 @@ namespace AideDeJeu.Views
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class MainPage : MasterDetailPage //TabbedPage
     {
-		public MainPage ()
+        ItemsViewModel viewModel;
+
+        public MainPage ()
 		{
 			InitializeComponent ();
-		}
-	}
+
+            BindingContext = viewModel = new ItemsViewModel();
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var item = args.SelectedItem as Item;
+            if (item == null)
+                return;
+
+            await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+        }
+
+        async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Items.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
 }
