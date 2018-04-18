bdc0607285eb2f7bbf735ce0b9ff87ff4d2563f0
Yan Maniez <yanmaniez@hotmail.com>
2018-04-18 22:28:45 +0200

    Reprise du modèle de p-fr en MasterDetailPage

--- AideDeJeu/AideDeJeu/Views/MainPage.xaml.cs
+++ AideDeJeu/AideDeJeu/Views/MainPage.xaml.cs
@@ -6,8 +6,8 @@ using Xamarin.Forms.Xaml;
 namespace AideDeJeu.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MainPage : TabbedPage
-	{
+	public partial class MainPage : MasterDetailPage //TabbedPage
+    {
 		public MainPage ()
 		{
 			InitializeComponent ();
