e832b896e8610f082a40b6d67b77ad1d2d913069
Yan Maniez <yanmaniez@hotmail.com>
2018-05-21 18:22:00 +0200

    Icone launcher

--- AideDeJeu/AideDeJeu.Android/MainActivity.cs
+++ AideDeJeu/AideDeJeu.Android/MainActivity.cs
@@ -10,7 +10,7 @@ using Android.OS;
 namespace AideDeJeu.Droid
 {
     //[Activity(Label = "Aide de Jeu", Icon = "@drawable/black_book", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    [Activity(Label = "Aide de Jeu", Icon = "@drawable/black_book", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Aide de Jeu", Icon = "@drawable/ic_launcher", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
