bfe8f0d81fad38623fe5fab7339138fd690917f9
yassine benabbas <yostane@yahoo.fr>
2018-05-30 13:39:39 +0200

    Version iOS fonctionnelle

--- AideDeJeu/AideDeJeu.iOS/NativeAPI.cs
+++ AideDeJeu/AideDeJeu.iOS/NativeAPI.cs
@@ -6,27 +6,26 @@ using Foundation;
 [assembly: Xamarin.Forms.Dependency(typeof(AideDeJeu.Droid.Version_Android))]
 namespace AideDeJeu.Droid
 {
-	public class Version_Android : INativeAPI
-	{
-		public string GetVersion()
-		{
+    public class Version_Android : INativeAPI
+    {
+        public string GetVersion()
+        {
 
-			return NSBundle.MainBundle.InfoDictionary[new NSString("CFBundleShortVersionString")].ToString();
-		}
+            return NSBundle.MainBundle.InfoDictionary[new NSString("CFBundleShortVersionString")].ToString();
+        }
 
-		public int GetBuild()
-		{
-			var buildVersion = NSBundle.MainBundle.InfoDictionary[new NSString("CFBundleVersion")].ToString();
-			int build = 0;
-			var res = int.TryParse(buildVersion, out build);
-			return res ? build : 0;
-		}
+        public int GetBuild()
+        {
+            var buildVersion = NSBundle.MainBundle.InfoDictionary[new NSString("CFBundleVersion")].ToString();
+            int build = 0;
+            var res = int.TryParse(buildVersion, out build);
+            return res ? build : 0;
+        }
 
-		public string GetDatabasePath(string databaseName)
-		{
-			SQLitePCL.Batteries_V2.Init();
-			var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library", databaseName);
-			return databasePath;
-		}
-	}
+        public string GetDatabasePath(string databaseName)
+        {
+            var databasePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "..", "Library", databaseName);
+            return databasePath;
+        }
+    }
 }
