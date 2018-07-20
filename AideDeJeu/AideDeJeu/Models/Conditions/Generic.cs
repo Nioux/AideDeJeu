99efb04f4b787e7591cd6c428e34c81230a42522
Yan Maniez <nioux59@outlook.com>
2018-07-20 13:54:13 +0200

    Test renderer

--- AideDeJeu/AideDeJeu/Models/Conditions/Generic.cs
+++ AideDeJeu/AideDeJeu/Models/Conditions/Generic.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using AideDeJeu.Tools;
+using Markdig;
+using Markdig.Renderers.Normalize;
 using Markdig.Syntax;
 
 namespace AideDeJeuLib
@@ -101,6 +104,14 @@ namespace AideDeJeuLib
                     }
                     else
                     {
+                        //var pipeline = new MarkdownPipelineBuilder().Build();
+
+                        //var writer = new StringWriter();
+                        //var renderer = new NormalizeRenderer(writer);
+                        //pipeline.Setup(renderer);
+
+                        //renderer.Render(listBlock);
+
                         int index = 0;
                         foreach (var inblock in listBlock)
                         {
