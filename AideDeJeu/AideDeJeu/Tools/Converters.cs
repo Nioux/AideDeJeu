55f522cb55b7f051264c6bca5dc252c258daf074
Yan Maniez <yanmaniez@hotmail.com>
2018-08-05 22:10:56 +0200

    Style variable en fonction du header

--- AideDeJeu/AideDeJeu/Tools/Converters.cs
+++ AideDeJeu/AideDeJeu/Tools/Converters.cs
@@ -33,4 +33,23 @@ namespace AideDeJeu.Tools
             return null;
         }
     }
+
+    public class HeaderLevelToStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var level = value as int?;
+            if(level.HasValue)
+            {
+                return Application.Current.Resources[$"heading{level.Value+1}"];
+            }
+            return Application.Current.Resources["paragraph"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+
 }
