64d7398275068ffbe9319d56f2017701a065ac3d
Yan Maniez <yanmaniez@hotmail.com>
2018-08-27 09:29:49 +0200

    Icones

--- AideDeJeu/AideDeJeu.Android/Resources/Resource.designer.cs
+++ AideDeJeu/AideDeJeu.Android/Resources/Resource.designer.cs
@@ -2192,26 +2192,26 @@ namespace AideDeJeu.Droid
 			// aapt resource value: 0x7f020053
 			public const int avd_hide_password = 2130837587;
 			
-			// aapt resource value: 0x7f020116
-			public const int avd_hide_password_1 = 2130837782;
-			
-			// aapt resource value: 0x7f020117
-			public const int avd_hide_password_2 = 2130837783;
-			
 			// aapt resource value: 0x7f020118
-			public const int avd_hide_password_3 = 2130837784;
+			public const int avd_hide_password_1 = 2130837784;
+			
+			// aapt resource value: 0x7f020119
+			public const int avd_hide_password_2 = 2130837785;
+			
+			// aapt resource value: 0x7f02011a
+			public const int avd_hide_password_3 = 2130837786;
 			
 			// aapt resource value: 0x7f020054
 			public const int avd_show_password = 2130837588;
 			
-			// aapt resource value: 0x7f020119
-			public const int avd_show_password_1 = 2130837785;
-			
-			// aapt resource value: 0x7f02011a
-			public const int avd_show_password_2 = 2130837786;
-			
 			// aapt resource value: 0x7f02011b
-			public const int avd_show_password_3 = 2130837787;
+			public const int avd_show_password_1 = 2130837787;
+			
+			// aapt resource value: 0x7f02011c
+			public const int avd_show_password_2 = 2130837788;
+			
+			// aapt resource value: 0x7f02011d
+			public const int avd_show_password_3 = 2130837789;
 			
 			// aapt resource value: 0x7f020055
 			public const int black_book = 2130837589;
@@ -2771,11 +2771,11 @@ namespace AideDeJeu.Droid
 			// aapt resource value: 0x7f02010e
 			public const int notification_icon_background = 2130837774;
 			
-			// aapt resource value: 0x7f020114
-			public const int notification_template_icon_bg = 2130837780;
+			// aapt resource value: 0x7f020116
+			public const int notification_template_icon_bg = 2130837782;
 			
-			// aapt resource value: 0x7f020115
-			public const int notification_template_icon_low_bg = 2130837781;
+			// aapt resource value: 0x7f020117
+			public const int notification_template_icon_low_bg = 2130837783;
 			
 			// aapt resource value: 0x7f02010f
 			public const int notification_tile_bg = 2130837775;
@@ -2784,13 +2784,19 @@ namespace AideDeJeu.Droid
 			public const int notify_panel_notification_icon_bg = 2130837776;
 			
 			// aapt resource value: 0x7f020111
-			public const int spell_book = 2130837777;
+			public const int round_star = 2130837777;
 			
 			// aapt resource value: 0x7f020112
-			public const int splash = 2130837778;
+			public const int spell_book = 2130837778;
 			
 			// aapt resource value: 0x7f020113
-			public const int wooden_sign = 2130837779;
+			public const int splash = 2130837779;
+			
+			// aapt resource value: 0x7f020114
+			public const int stars_stack = 2130837780;
+			
+			// aapt resource value: 0x7f020115
+			public const int wooden_sign = 2130837781;
 			
 			static Drawable()
 			{
