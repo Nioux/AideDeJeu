9dc92b38afc60573ae8b8a6467222bd9346bf33f
Yan Maniez <nioux59@outlook.com>
2018-10-02 13:05:49 +0200

    Logo uwp

--- Data/Icons/convertmain.bat
+++
@@ -1,5 +0,0 @@
-set INKSCAPE=..\..\Ignore\inkscape\inkscape.com
-set ANDROID_PATH=..\..\AideDeJeu\AideDeJeu.Android\Resources\
-set UWP_PATH=..\..\AideDeJeu\AideDeJeu.UWP\
-
-%INKSCAPE% -z -e %1 -a -264:0:784:512 -w 620 -h 300 -b #ffffff %2
---
+++ Data/Icons/convertuwp.bat
@@ -0,0 +1,44 @@
+set INKSCAPE=..\..\Ignore\inkscape\inkscape.com
+set ANDROID_PATH=..\..\AideDeJeu\AideDeJeu.Android\Resources\
+set UWP_PATH=..\..\AideDeJeu\AideDeJeu.UWP\
+
+rem %INKSCAPE% -z -a -272:0:790:512 -w 620 -h 300 -b #ffffff -e %1 %2
+
+
+%INKSCAPE% -z -a 0:0:512:512 -w 24 -h 24 -e %UWP_PATH%Assets\LockScreenLogo.scale-100.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 30 -h 30 -e %UWP_PATH%Assets\LockScreenLogo.scale-125.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 36 -h 36 -e %UWP_PATH%Assets\LockScreenLogo.scale-150.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 48 -h 48 -e %UWP_PATH%Assets\LockScreenLogo.scale-200.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 96 -h 96 -e %UWP_PATH%Assets\LockScreenLogo.scale-400.png battle-axe-white.svg
+
+%INKSCAPE% -z -a -272:0:790:512 -w 620 -h 300 -e %UWP_PATH%Assets\SplashScreen.scale-100.png battle-axe-white.svg
+%INKSCAPE% -z -a -272:0:790:512 -w 775 -h 375 -e %UWP_PATH%Assets\SplashScreen.scale-125.png battle-axe-white.svg
+%INKSCAPE% -z -a -272:0:790:512 -w 930 -h 450 -e %UWP_PATH%Assets\SplashScreen.scale-150.png battle-axe-white.svg
+%INKSCAPE% -z -a -272:0:790:512 -w 1240 -h 600 -e %UWP_PATH%Assets\SplashScreen.scale-200.png battle-axe-white.svg
+%INKSCAPE% -z -a -272:0:790:512 -w 2480 -h 1200 -e %UWP_PATH%Assets\SplashScreen.scale-400.png battle-axe-white.svg
+
+%INKSCAPE% -z -a 0:0:512:512 -w 44 -h 44 -e %UWP_PATH%Assets\Square150x150Logo.scale-100.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 55 -h 55 -e %UWP_PATH%Assets\Square150x150Logo.scale-125.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 66 -h 66 -e %UWP_PATH%Assets\Square150x150Logo.scale-150.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 88 -h 88 -e %UWP_PATH%Assets\Square150x150Logo.scale-200.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 176 -h 176 -e %UWP_PATH%Assets\Square150x150Logo.scale-400.png battle-axe-white.svg
+
+%INKSCAPE% -z -a 0:0:512:512 -w 16 -h 16 -e %UWP_PATH%Assets\Square44x44Logo.scale-100.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 24 -h 24 -e %UWP_PATH%Assets\Square44x44Logo.scale-125.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 32 -h 32 -e %UWP_PATH%Assets\Square44x44Logo.scale-150.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 48 -h 48 -e %UWP_PATH%Assets\Square44x44Logo.scale-200.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 256 -h 256 -e %UWP_PATH%Assets\Square44x44Logo.scale-400.png battle-axe-white.svg
+
+%INKSCAPE% -z -a 0:0:512:512 -w 150 -h 150 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-16_altform-unplated.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 188 -h 188 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-24_altform-unplated.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 225 -h 225 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-32_altform-unplated.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 300 -h 300 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-48_altform-unplated.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 600 -h 600 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-256_altform-unplated.png battle-axe-white.svg
+
+%INKSCAPE% -z -a 0:0:512:512 -w 50 -h 50 -e %UWP_PATH%Assets\StoreLogo.png battle-axe-white.svg
+
+%INKSCAPE% -z -a -272:0:790:512 -w 310 -h 150 -e %UWP_PATH%Assets\Wide310x150Logo.scale-100.png battle-axe-white.svg
+%INKSCAPE% -z -a -272:0:790:512 -w 388 -h 188 -e %UWP_PATH%Assets\Wide310x150Logo.scale-125.png battle-axe-white.svg
+%INKSCAPE% -z -a -272:0:790:512 -w 465 -h 225 -e %UWP_PATH%Assets\Wide310x150Logo.scale-150.png battle-axe-white.svg
+%INKSCAPE% -z -a -272:0:790:512 -w 620 -h 300 -e %UWP_PATH%Assets\Wide310x150Logo.scale-200.png battle-axe-white.svg
+%INKSCAPE% -z -a -272:0:790:512 -w 1240 -h 600 -e %UWP_PATH%Assets\Wide310x150Logo.scale-400.png battle-axe-white.svg
