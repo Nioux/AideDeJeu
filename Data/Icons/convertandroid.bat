dbdc0c4f3354185f02788cc00a6a927f0dc0475a
Yan Maniez <nioux59@outlook.com>
2018-10-03 14:00:18 +0200

    Logos / Captures

---
+++ Data/Icons/convertandroid.bat
@@ -0,0 +1,7 @@
+set INKSCAPE=..\..\Ignore\inkscape\inkscape.com
+set ANDROID_PATH=..\..\AideDeJeu\AideDeJeu.Android\
+
+%INKSCAPE% -z -a -272:0:790:512 -w 1024 -h 500 -b #ffffff -e %ANDROID_PATH%icon1024x500.png battle-axe.svg
+
+%INKSCAPE% -z -a 0:0:512:512 -w 512 -h 512 -b #ffffff -e %ANDROID_PATH%icon512.png battle-axe.svg
+
