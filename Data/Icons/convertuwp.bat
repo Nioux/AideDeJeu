3bbf07a209c79ed57e32233a77be720929537cd7
Yan Maniez <yanmaniez@hotmail.com>
2018-10-03 21:36:14 +0200

    v1.28 UWP

--- Data/Icons/convertuwp.bat
+++ Data/Icons/convertuwp.bat
@@ -17,23 +17,23 @@ rem %INKSCAPE% -z -a -272:0:790:512 -w 620 -h 300 -b #ffffff -e %1 %2
 %INKSCAPE% -z -a -272:0:790:512 -w 1240 -h 600 -e %UWP_PATH%Assets\SplashScreen.scale-200.png battle-axe-white.svg
 %INKSCAPE% -z -a -272:0:790:512 -w 2480 -h 1200 -e %UWP_PATH%Assets\SplashScreen.scale-400.png battle-axe-white.svg
 
-%INKSCAPE% -z -a 0:0:512:512 -w 44 -h 44 -e %UWP_PATH%Assets\Square150x150Logo.scale-100.png battle-axe-white.svg
-%INKSCAPE% -z -a 0:0:512:512 -w 55 -h 55 -e %UWP_PATH%Assets\Square150x150Logo.scale-125.png battle-axe-white.svg
-%INKSCAPE% -z -a 0:0:512:512 -w 66 -h 66 -e %UWP_PATH%Assets\Square150x150Logo.scale-150.png battle-axe-white.svg
-%INKSCAPE% -z -a 0:0:512:512 -w 88 -h 88 -e %UWP_PATH%Assets\Square150x150Logo.scale-200.png battle-axe-white.svg
-%INKSCAPE% -z -a 0:0:512:512 -w 176 -h 176 -e %UWP_PATH%Assets\Square150x150Logo.scale-400.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 150 -h 150 -e %UWP_PATH%Assets\Square150x150Logo.scale-100.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 188 -h 188 -e %UWP_PATH%Assets\Square150x150Logo.scale-125.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 225 -h 225 -e %UWP_PATH%Assets\Square150x150Logo.scale-150.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 300 -h 300 -e %UWP_PATH%Assets\Square150x150Logo.scale-200.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 600 -h 600 -e %UWP_PATH%Assets\Square150x150Logo.scale-400.png battle-axe-white.svg
 
-%INKSCAPE% -z -a 0:0:512:512 -w 16 -h 16 -e %UWP_PATH%Assets\Square44x44Logo.scale-100.png battle-axe-white.svg
-%INKSCAPE% -z -a 0:0:512:512 -w 24 -h 24 -e %UWP_PATH%Assets\Square44x44Logo.scale-125.png battle-axe-white.svg
-%INKSCAPE% -z -a 0:0:512:512 -w 32 -h 32 -e %UWP_PATH%Assets\Square44x44Logo.scale-150.png battle-axe-white.svg
-%INKSCAPE% -z -a 0:0:512:512 -w 48 -h 48 -e %UWP_PATH%Assets\Square44x44Logo.scale-200.png battle-axe-white.svg
-%INKSCAPE% -z -a 0:0:512:512 -w 256 -h 256 -e %UWP_PATH%Assets\Square44x44Logo.scale-400.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 44 -h 44 -e %UWP_PATH%Assets\Square44x44Logo.scale-100.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 55 -h 55 -e %UWP_PATH%Assets\Square44x44Logo.scale-125.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 66 -h 66 -e %UWP_PATH%Assets\Square44x44Logo.scale-150.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 88 -h 88 -e %UWP_PATH%Assets\Square44x44Logo.scale-200.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 176 -h 176 -e %UWP_PATH%Assets\Square44x44Logo.scale-400.png battle-axe-white.svg
 
-%INKSCAPE% -z -a 0:0:512:512 -w 150 -h 150 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-16_altform-unplated.png battle-axe-white.svg
-%INKSCAPE% -z -a 0:0:512:512 -w 188 -h 188 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-24_altform-unplated.png battle-axe-white.svg
-%INKSCAPE% -z -a 0:0:512:512 -w 225 -h 225 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-32_altform-unplated.png battle-axe-white.svg
-%INKSCAPE% -z -a 0:0:512:512 -w 300 -h 300 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-48_altform-unplated.png battle-axe-white.svg
-%INKSCAPE% -z -a 0:0:512:512 -w 600 -h 600 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-256_altform-unplated.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 16 -h 16 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-16_altform-unplated.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 24 -h 24 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-24_altform-unplated.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 32 -h 32 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-32_altform-unplated.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 48 -h 48 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-48_altform-unplated.png battle-axe-white.svg
+%INKSCAPE% -z -a 0:0:512:512 -w 256 -h 256 -e %UWP_PATH%Assets\Square44x44Logo.targetsize-256_altform-unplated.png battle-axe-white.svg
 
 %INKSCAPE% -z -a 0:0:512:512 -w 50 -h 50 -e %UWP_PATH%Assets\StoreLogo.png battle-axe-white.svg
 
