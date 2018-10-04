adddd1008cef2c7ac0a0e825ef8bcf9caedf169a
Yan Maniez <yanmaniez@hotmail.com>
2018-10-04 20:15:10 +0200

    Test GTK#

---
+++ AideDeJeu/AideDeJeu.GTK/NativeAPI.cs
@@ -0,0 +1,63 @@
+﻿using AideDeJeu.Tools;
+using System;
+using System.IO;
+
+[assembly: Xamarin.Forms.Dependency(typeof(AideDeJeu.GTK.Version_GTK))]
+namespace AideDeJeu.GTK
+{
+    public class Version_GTK : INativeAPI
+    {
+        public string GetVersion()
+        {
+            return "";
+            //var context = global::Android.App.Application.Context;
+
+            //PackageManager manager = context.PackageManager;
+            //PackageInfo info = manager.GetPackageInfo(context.PackageName, 0);
+
+            //return info.VersionName;
+        }
+
+        public int GetBuild()
+        {
+            return 0;
+            //var context = global::Android.App.Application.Context;
+            //PackageManager manager = context.PackageManager;
+            //PackageInfo info = manager.GetPackageInfo(context.PackageName, 0);
+
+            //return info.VersionCode;
+        }
+
+        public string GetDatabasePath(string databaseName)
+        {
+            return null;
+            //var documentsDirectoryPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            //var path = Path.Combine(documentsDirectoryPath, databaseName);
+
+            //// This is where we copy in our pre-created database
+            //if (!File.Exists(path))
+            //{
+            //    using (var inStream = Android.App.Application.Context.Assets.Open(databaseName))
+            //    {
+            //        using (var outStream = new FileStream(path, FileMode.Create))
+            //        {
+            //            inStream.CopyTo(outStream);
+            //        }
+            //    }
+            //    //using (var binaryReader = new BinaryReader(Android.App.Application.Context.Assets.Open(databaseName)))
+            //    //{
+            //    //    using (var binaryWriter = new BinaryWriter(new FileStream(path, FileMode.Create)))
+            //    //    {
+            //    //        byte[] buffer = new byte[2048];
+            //    //        int length = 0;
+            //    //        while ((length = binaryReader.Read(buffer, 0, buffer.Length)) > 0)
+            //    //        {
+            //    //            binaryWriter.Write(buffer, 0, length);
+            //    //        }
+            //    //    }
+            //    //}
+            //}
+            //return path;
+        }
+    }
+}
---
+++ AideDeJeu/AideDeJeu.GTK/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.GTK;
+
+namespace AideDeJeu.GTK
+{
+    class MainClass
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Gtk.Application.Init();
+            Forms.Init();
+
+            var app = new App();
+            var window = new FormsWindow();
+            window.LoadApplication(app);
+            window.SetApplicationTitle("Haches & Dés");
+            window.Show();
+
+            Gtk.Application.Run();
+        }
+    }
+}
