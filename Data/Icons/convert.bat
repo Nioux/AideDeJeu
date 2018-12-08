7deab73acd84c15967a6406ea06529cfbb024a83
Maniez Yan <FR19960@fr01.awl.atosorigin.net>
2018-12-09 00:42:22 +0100

    Images iOS

--- Data/Icons/convert.bat
+++ Data/Icons/convert.bat
@@ -1,29 +1,29 @@
 
-goto nouveau
+rem goto nouveau
 
-call convertone.bat crystal_ball.png crystal-ball.svg
+call convertone.bat crystal_ball crystal-ball
 
-call convertone.bat funnel.png funnel.svg
+call convertone.bat funnel funnel
 
-call convertone.bat round_star.png round-star.svg
+call convertone.bat round_star round-star
 
-call convertone.bat settings_knobs.png settings-knobs.svg
+call convertone.bat settings_knobs settings-knobs
 
-call convertone.bat spell_book.png spell-book.svg
+call convertone.bat spell_book spell-book
 
-call convertone.bat stars_stack.png stars-stack.svg
+call convertone.bat stars_stack stars-stack
 
-call convertone.bat wooden_sign.png wooden-sign.svg
+call convertone.bat wooden_sign wooden-sign
 
-call convertone.bat plain_arrow_down.png plain-arrow-down.svg
+call convertone.bat plain_arrow_down plain-arrow-down
 
-call convertone.bat plain_arrow_up.png plain-arrow-up.svg
+call convertone.bat plain_arrow_up plain-arrow-up
 
-call convertone.bat trash_can.png trash-can.svg
+call convertone.bat trash_can trash-can
 
-call convertone.bat battle_axe.png battle-axe.svg
+rem call convertone.bat battle_axe battle-axe
 
 :nouveau
-call convertmain.bat battle_axe.png battle-axe.svg
+rem call convertmain.bat battle_axe.png battle-axe.svg
 
 pause
--- Data/Icons/convertone.bat
+++ Data/Icons/convertone.bat
@@ -1,10 +1,22 @@
 set INKSCAPE=..\..\Ignore\inkscape\inkscape.com
 set ANDROID_PATH=..\..\AideDeJeu\AideDeJeu.Android\Resources\
 set UWP_PATH=..\..\AideDeJeu\AideDeJeu.UWP\
+set IOS_PATH=..\..\AideDeJeu\AideDeJeu.iOS\Media.xcassets\
 
-%INKSCAPE% -z -e %ANDROID_PATH%drawable\%1 -w 72 -h 72 %2
-%INKSCAPE% -z -e %ANDROID_PATH%drawable-hdpi\%1 -w 72 -h 72 %2
-%INKSCAPE% -z -e %ANDROID_PATH%drawable-xhdpi\%1 -w 96 -h 96 %2
-%INKSCAPE% -z -e %ANDROID_PATH%drawable-xxhdpi\%1 -w 144 -h 144 %2
-%INKSCAPE% -z -e %UWP_PATH%%1 -w 144 -h 144 %2
+goto ios
 
+:android
+
+%INKSCAPE% -z -e %ANDROID_PATH%drawable\%1.png -w 72 -h 72 %2.svg
+%INKSCAPE% -z -e %ANDROID_PATH%drawable-hdpi\%1.png -w 72 -h 72 %2.svg
+%INKSCAPE% -z -e %ANDROID_PATH%drawable-xhdpi\%1.png -w 96 -h 96 %2.svg
+%INKSCAPE% -z -e %ANDROID_PATH%drawable-xxhdpi\%1.png -w 144 -h 144 %2.svg
+
+:uwp
+
+%INKSCAPE% -z -e %UWP_PATH%%1.png -w 144 -h 144 %2.svg
+
+:ios
+
+md %IOS_PATH%%1.imageset\
+%INKSCAPE% -z -e %IOS_PATH%%1.imageset\%1.png -w 69 -h 69 %2.svg
