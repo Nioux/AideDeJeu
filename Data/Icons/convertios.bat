25d6de70dca503248f5d85e4ae1492310fc8558e
Maniez Yan <FR19960@fr01.awl.atosorigin.net>
2018-12-09 01:39:38 +0100

    Icone principale iOS

---
+++ Data/Icons/convertios.bat
@@ -0,0 +1,18 @@
+set INKSCAPE=..\..\Ignore\inkscape\inkscape.com
+set IOS_PATH=..\..\AideDeJeu\AideDeJeu.iOS\Media.xcassets\
+
+%INKSCAPE% -w 512 -h 512 -b #ffffff -A %IOS_PATH%icon.imageset\battle_axe.pdf battle-axe.svg
+
+%INKSCAPE% -w 20 -h 20 -b #ffffff -e %IOS_PATH%AppIcon.appiconset\Icon-20.png battle-axe.svg
+%INKSCAPE% -w 29 -h 29 -b #ffffff -e %IOS_PATH%AppIcon.appiconset\Icon-29.png battle-axe.svg
+%INKSCAPE% -w 40 -h 40 -b #ffffff -e %IOS_PATH%AppIcon.appiconset\Icon-40.png battle-axe.svg
+%INKSCAPE% -w 58 -h 58 -b #ffffff -e %IOS_PATH%AppIcon.appiconset\Icon-58.png battle-axe.svg
+%INKSCAPE% -w 60 -h 60 -b #ffffff -e %IOS_PATH%AppIcon.appiconset\Icon-60.png battle-axe.svg
+%INKSCAPE% -w 76 -h 76 -b #ffffff -e %IOS_PATH%AppIcon.appiconset\Icon-76.png battle-axe.svg
+%INKSCAPE% -w 80 -h 80 -b #ffffff -e %IOS_PATH%AppIcon.appiconset\Icon-80.png battle-axe.svg
+%INKSCAPE% -w 87 -h 87 -b #ffffff -e %IOS_PATH%AppIcon.appiconset\Icon-87.png battle-axe.svg
+%INKSCAPE% -w 120 -h 120 -b #ffffff -e %IOS_PATH%AppIcon.appiconset\Icon-120.png battle-axe.svg
+%INKSCAPE% -w 152 -h 152 -b #ffffff -e %IOS_PATH%AppIcon.appiconset\Icon-152.png battle-axe.svg
+%INKSCAPE% -w 167 -h 167 -b #ffffff -e %IOS_PATH%AppIcon.appiconset\Icon-167.png battle-axe.svg
+%INKSCAPE% -w 180 -h 180 -b #ffffff -e %IOS_PATH%AppIcon.appiconset\Icon-180.png battle-axe.svg
+%INKSCAPE% -w 1024 -h 1024 -b #ffffff -e %IOS_PATH%AppIcon.appiconset\Icon-1024.png battle-axe.svg
