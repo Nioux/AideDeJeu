2a325ce406890409bf4251d33f99b6cdb6fecf0c
Yan Maniez <yan.maniez@atos.net>
2019-05-07 17:17:31 +0200

    Grid

---
+++ AideDeJeu/AideDeJeu/Views/PlayerCharacter/AbilityBaseValueView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AideDeJeu.ViewModels.PlayerCharacter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AideDeJeu.Views.PlayerCharacter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AbilityBaseValueView : ContentView
+    {
+        public AbilityBaseValueView()
+        {
+            InitializeComponent();
+        }
+
+        public AbilityViewModel Ability
+        {
+            get { return (AbilityViewModel)GetValue(AbilityProperty); }
+            set { SetValue(AbilityProperty, value); }
+        }
+        public static readonly BindableProperty AbilityProperty = BindableProperty.Create(
+            nameof(Ability),
+            typeof(AbilityViewModel),
+            typeof(AbilityBaseValueView),
+            defaultValue: default(AbilityViewModel));
+    }
+}
---
+++ AideDeJeu/AideDeJeu/Views/PlayerCharacter/AbilityRacialBonusView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AideDeJeu.ViewModels.PlayerCharacter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AideDeJeu.Views.PlayerCharacter
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AbilityRacialBonusView : ContentView
+    {
+        public AbilityRacialBonusView()
+        {
+            InitializeComponent();
+        }
+
+        public AbilityViewModel Ability
+        {
+            get { return (AbilityViewModel)GetValue(AbilityProperty); }
+            set { SetValue(AbilityProperty, value); }
+        }
+        public static readonly BindableProperty AbilityProperty = BindableProperty.Create(
+            nameof(Ability),
+            typeof(AbilityViewModel),
+            typeof(AbilityRacialBonusView),
+            defaultValue: default(AbilityViewModel));
+    }
+}
