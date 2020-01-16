e91d01738f364e076294399731510ae279f0fb21
Yan Maniez <yanmaniez@hotmail.com>
2020-01-17 00:25:40 +0100

    Suite Osgild

--- Docs/Osgild/scripts.js
+++ Docs/Osgild/scripts.js
@@ -5,4 +5,17 @@ function init() {
 	range.oninput = function() {
 		map.style.width = "" + range.value + "%";
 	}
+	//range.addEventListener("wheel", onwheel);
+}
+
+function onwheel() {
+	var range = document.getElementById("range");
+	var map = document.getElementById("map");
+	//alert(range.value);
+	//var percent = range.value.toString().substring(0, range.length - 1);
+	//alert(percent);
+	range.value = range.value + 10;
+	map.style.width = "" + range.value + "%";
+	//map.style.width = "" + (parseInt(range.value) + 10) + "%";
+	console.log(range.value);
 }
