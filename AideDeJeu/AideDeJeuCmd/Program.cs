ee3ec7b9fa1d1a6e4d45f93d7491d003227963f6
Yan Maniez <nioux59@outlook.com>
2020-02-05 20:32:39 +0100

    Lib JoA

--- AideDeJeu/AideDeJeuCmd/Program.cs
+++ AideDeJeu/AideDeJeuCmd/Program.cs
@@ -1111,7 +1111,7 @@ namespace AideDeJeuCmd
                     {
                         Console.WriteLine(filename);
                     }
-                    await SaveStringAsync(filename, yaml);
+                    //await SaveStringAsync(filename, yaml);
 
                     var filtervm = item.GetNewFilterViewModel();
                     if (filtervm != null)
